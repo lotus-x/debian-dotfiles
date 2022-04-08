9198f10f0717ee7922a54bad543256c8382501c4
lotus-x <isuruimesh@gmail.com>
2022-04-08 19:33:35 +0530

    Added zpresto

--- pre-config.sh
+++ pre-config.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-apt install -y zsh
+apt install -y zsh git
+chsh -s /bin/zsh
 
 sudo add-apt-repository universe
 apt install -y powerline
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
